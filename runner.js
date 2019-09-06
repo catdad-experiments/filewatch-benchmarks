2690dbc3a34e095c28cbbea2234727d42d5f637c
Kiril Vatev <vatev.1@gmail.com>
2019-09-06 09:54:11 -0400

    cleanup

--- runner.js
+++ runner.js
@@ -7,7 +7,6 @@ const root = require('rootrequire');
 const run = require('grandma/lib/run');
 const report = require('grandma/lib/report');
 const through = require('through2');
-const streamify = require('stream-array');
 
 const grandma = { run, report };
 
@@ -15,42 +14,27 @@ const files = fs.readdirSync(path.resolve(root, 'grandma'))
 .filter(f => /\.js$/.test(f))
 .map(f => path.resolve(root, 'grandma', f));
 
-process.on('unhandledRejection', (reason, promise) => {
-  console.log('rejected promise');
-  console.log(reason);
-  console.log(promise);
-});
-
-setTimeout(() => {
-  console.log('!!!! timeout happened');
-}, 25000);
-
 (async function () {
   for (let file of files) {
     console.log('running', file);
 
-    const data = [];
-    const temp = through();
+    const data = through();
 
-    temp.on('data', chunk => data.push(chunk));
-    temp.on('end', () => {});
-
-    await promisify(grandma.run)({
-      duration: 10, // TODO get from file
-      concurrent: 1,
-      test: {
-        path: file,
-        name: path.basename(file).replace(path.extname(file), '')
-      },
-      output: temp
-    });
-
-    console.log('!!!! finished running');
-
-    const result = await promisify(grandma.report)({
-      type: 'json',
-      input: streamify(data)
-    });
+    const [ , result ] = await Promise.all([
+      promisify(grandma.run)({
+        duration: '30s', // TODO get from file
+        concurrent: 1,
+        test: {
+          path: file,
+          name: path.basename(file).replace(path.extname(file), '')
+        },
+        output: data
+      }),
+      promisify(grandma.report)({
+        type: 'json',
+        input: data
+      })
+    ]);
 
     console.log(result);
   }
