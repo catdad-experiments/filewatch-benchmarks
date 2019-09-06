953ddbecfe4899e7364832c31f025556cfa09c97
Kiril Vatev <vatev.1@gmail.com>
2019-09-06 10:17:13 -0400

    printing pretty table of results

--- runner.js
+++ runner.js
@@ -4,29 +4,51 @@ const path = require('path');
 const { promisify } = require('util');
 
 const root = require('rootrequire');
-const run = require('grandma/lib/run');
-const report = require('grandma/lib/report');
+const grandma = require('grandma');
 const through = require('through2');
+const bytes = require('pretty-bytes');
+const table = require('fancy-text-table');
 
-const grandma = { run, report };
+const time = n => `${n.toFixed(2)}ms`;
 
 const files = fs.readdirSync(path.resolve(root, 'grandma'))
 .filter(f => /\.js$/.test(f))
 .map(f => path.resolve(root, 'grandma', f));
 
+function filesResultsSerializer(name, categories) {
+  const tbl = table();
+
+  tbl.row(['', 'mean', '95th percentile']);
+
+  Object.keys(categories).forEach(lib => {
+    const metrics = categories[lib].metrics;
+
+    tbl.line();
+    tbl.title(`${lib} ${name}, ${categories[lib].info.count} iterations`);
+    tbl.row(['ready in', time(metrics['time to ready'].mean), time(metrics['time to ready'][95])]);
+    tbl.row(['total heap', bytes(metrics['heap total'].mean), bytes(metrics['heap total'][95])]);
+    tbl.row(['total rss', bytes(metrics['rss total'].mean), bytes(metrics['rss total'][95])]);
+    tbl.row(['retained heap', bytes(metrics['heap retained'].mean), bytes(metrics['heap retained'][95])]);
+    tbl.row(['retained rss', bytes(metrics['rss retained'].mean), bytes(metrics['rss retained'][95])]);
+  });
+
+  console.log(tbl.render());
+}
+
 (async function () {
   for (let file of files) {
-    console.log('running', file);
+    const name = path.basename(file).replace(path.extname(file), '');
+    console.log(`running ${name}...`);
 
     const data = through();
 
-    const [ , result ] = await Promise.all([
+    const [ , { categories } ] = await Promise.all([
       promisify(grandma.run)({
         duration: '30s', // TODO get from file
         concurrent: 1,
         test: {
           path: file,
-          name: path.basename(file).replace(path.extname(file), '')
+          name: name
         },
         output: data
       }),
@@ -36,11 +58,11 @@ const files = fs.readdirSync(path.resolve(root, 'grandma'))
       })
     ]);
 
-    console.log(result);
+    filesResultsSerializer(name, categories);
   }
 })().then(() => {
-  console.log('Finished successfully');
+  console.log('\nFinished successfully');
 }).catch(e => {
-  console.error('Finished with error:', e);
+  console.error('\nFinished with error:', e);
   process.exitCode = 1;
 });
