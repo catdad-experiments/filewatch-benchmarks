d290d0ede04f317b4ced6e99c1921dd2fc01c197
Kiril Vatev <vatev.1@gmail.com>
2019-09-06 11:22:21 -0400

    adding padding character for result headings, extending longest test for more results

--- grandma/files-1x2000.js
+++ grandma/files-1x2000.js
@@ -2,5 +2,5 @@ module.exports = require('../lib/grandma-filetest-wrapper.js')({
   dirCount: 1,
   fileCount: 2000,
   libs: ['chokidar', 'gaze', 'watchboy'],
-  duration: '2m'
+  duration: '3m'
 });
