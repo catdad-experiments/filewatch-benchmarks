84ffcfe820dd5e79fcdc3c51454ee59486f027d2
Kiril Vatev <vatev.1@gmail.com>
2019-09-06 10:47:52 -0400

    prettier results

--- runner.js
+++ runner.js
@@ -8,6 +8,7 @@ const grandma = require('grandma');
 const through = require('through2');
 const bytes = require('pretty-bytes');
 const table = require('fancy-text-table');
+const chalk = require('chalk');
 
 const time = n => `${n.toFixed(2)}ms`;
 
@@ -17,19 +18,24 @@ const files = fs.readdirSync(path.resolve(root, 'grandma'))
 
 function filesResultsSerializer(name, categories) {
   const tbl = table();
+  tbl.line();
 
-  tbl.row(['', 'mean', '95th percentile']);
+  const title = str => tbl.title(chalk.magenta(str));
+  const row = arr => tbl.row([' ', ...arr]);
+  const headings = arr => row(arr.map(s => chalk.cyan.bold(s)));
+
+  headings(['', 'mean', '95th percentile']);
 
   Object.keys(categories).forEach(lib => {
     const metrics = categories[lib].metrics;
 
+    title(chalk.magenta(`${lib} ${name}, ${categories[lib].info.count} iterations`));
+    row(['ready in', time(metrics['time to ready'].mean), time(metrics['time to ready'][95])]);
+    row(['total heap', bytes(metrics['heap total'].mean), bytes(metrics['heap total'][95])]);
+    row(['total rss', bytes(metrics['rss total'].mean), bytes(metrics['rss total'][95])]);
+    row(['retained heap', bytes(metrics['heap retained'].mean), bytes(metrics['heap retained'][95])]);
+    row(['retained rss', bytes(metrics['rss retained'].mean), bytes(metrics['rss retained'][95])]);
     tbl.line();
-    tbl.title(`${lib} ${name}, ${categories[lib].info.count} iterations`);
-    tbl.row(['ready in', time(metrics['time to ready'].mean), time(metrics['time to ready'][95])]);
-    tbl.row(['total heap', bytes(metrics['heap total'].mean), bytes(metrics['heap total'][95])]);
-    tbl.row(['total rss', bytes(metrics['rss total'].mean), bytes(metrics['rss total'][95])]);
-    tbl.row(['retained heap', bytes(metrics['heap retained'].mean), bytes(metrics['heap retained'][95])]);
-    tbl.row(['retained rss', bytes(metrics['rss retained'].mean), bytes(metrics['rss retained'][95])]);
   });
 
   console.log(tbl.render());
@@ -37,8 +43,9 @@ function filesResultsSerializer(name, categories) {
 
 (async function () {
   for (let file of files) {
+    const test = require(file);
     const name = path.basename(file).replace(path.extname(file), '');
-    console.log(`running ${name}...`);
+    console.log(`running ${name} for ${test.duration}...`);
 
     const data = through();
 
@@ -61,8 +68,8 @@ function filesResultsSerializer(name, categories) {
     filesResultsSerializer(name, categories);
   }
 })().then(() => {
-  console.log('\nFinished successfully');
+  console.log('Finished successfully');
 }).catch(e => {
-  console.error('\nFinished with error:', e);
+  console.error('Finished with error:', e);
   process.exitCode = 1;
 });
